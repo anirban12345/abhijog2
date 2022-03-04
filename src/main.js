7431d2420c45c1058236cecd954931d2acd244de
anir12345 <anizrock2012@gmail.com>
2022-03-04 08:58:26 +0530

    .

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/assets/libs/chartist/dist/chartist.min.css'
-
-
-import './assets/assets/extra-libs/c3/c3.min.css'
-import './assets/assets/libs/morris.js/morris.css'
-    
-import './assets/dist/css/style.min.css'
-
-    
 
 createApp(App).mount('#app')
